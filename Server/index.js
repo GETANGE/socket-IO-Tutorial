a109ef350a06ed3a328955ca5a06d932dddec3d3
GETANGE <emmanuelgetange48@gmail.com>
2024-02-28 11:55:07 +0300

    ADD: basics of Sockets

--- Server/index.js
+++ Server/index.js
@@ -1,10 +1,33 @@
-import express from 'express';
-import morgan from 'morgan';
-import dotenv from 'dotenv';
+const express = require('express');
+const morgan = require('morgan');
+const dotenv = require('dotenv');
+const socketio = require('socket.io');
 
 const app = express();
-dotenv.config({path: 'config.env'});
+const port = 3000;
+dotenv.config({ path: 'config.env' });
 
-if(process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'development'){
+//rendering static files
+app.use(express.static(__dirname + '/public'));
+
+if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'));
 }
+
+// creating the server
+const expressServer = app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`);
+});
+
+// creating the the socket connection
+const io = socketio(expressServer);
+
+io.on('connection', (socket)=>{
+    console.log(socket.id, "has connected");
+    socket.emit('Message from server',{
+        data: 'Welcome to the Socket server'
+    })
+    socket.on('Message from client', (data)=>{
+        console.log("Data: ",data);
+    })
+})
